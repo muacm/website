dcc27bf3ec4efed5810330655691e1171d65ce98
yashsehgal <yashsehgal.work@gmail.com>
2021-05-11 01:17:09 +0530

    old files removed

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/Style.css'
-import './styles/footer.css'
-import './styles/index.css'
 import App from './App';
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Header />
     <App />
+    <Footer />
   </React.StrictMode>,
   document.getElementById('root')
 );
